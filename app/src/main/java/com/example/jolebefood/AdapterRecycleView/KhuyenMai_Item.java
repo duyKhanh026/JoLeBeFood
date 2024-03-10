a4ec8defb35b7c762224c2d2416d2be6318c9633
Danh <danh123098@gmail.com>
2024-03-10 09:42:25 +0700

    Thêm page Khuyến mãi

--- app/src/main/java/com/example/jolebefood/AdapterRecycleView/KhuyenMai_Item.java
+++ app/src/main/java/com/example/jolebefood/AdapterRecycleView/KhuyenMai_Item.java
@@ -1,6 +1,7 @@
 package com.example.jolebefood.AdapterRecycleView;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,18 +9,39 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.jolebefood.R;
 
-//public class KhuyenMai_Item extends RecyclerView.Adapter<KhuyenMai_Item.MyViewHolder> {
-//
-//    public class MyViewHolder extends RecyclerView.ViewHolder {
-//        TextView textView;
-//
-//        public MyViewHolder(@NonNull View itemView) {
-//            super(itemView);
-////            textView = itemView.findViewById(R.id.txtDanhMuc_LS);
-//        }
-//
-////        public void bindData(String data) {
-////            textView.setText(data);
-////        }
-//    }
-//}
+import java.util.ArrayList;
+
+public class KhuyenMai_Item extends RecyclerView.Adapter<KhuyenMai_Item.MyViewHolder> {
+
+    ArrayList<String> listkm;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.txtDanhMuc_LS);
+        }
+
+       public void bindData(String data) {
+            textView.setText(data);
+        }
+    }
+}
